0fc988320b0466b7a0ba68556bdd404e99015887
JiangYang0662 <741456914@qq.com>
2022-07-28 21:53:54 +0800

    项目配置-vant组件+rem适配器+icon图标+request模块

---
+++ .postcssrc.js
@@ -0,0 +1,22 @@
+// vant是基于375写的，而平常的设计图是根据750写的
+// 要根据不同的元素来设置不同的缩小值
+// vant --37.5
+// 设计图的元素 -- 75
+module.exports = {
+    plugins: {
+    // 和vue的配置重复了，需注释掉
+    //   'autoprefixer': {
+    //     browsers: ['Android >= 4.0', 'iOS >= 8']
+    //   },
+    // 1. 把px转换rem
+      'postcss-pxtorem': {
+        // rootValue支持数字和函数 number | function
+        // rootValue: 37.5,
+        rootValue ({ file }) {
+            return file.indexOf('vant') !== -1 ? 37.5 : 75
+          },
+          // *表示转换所有样式
+        propList: ['*']
+      }
+    }
+  }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载Vant核心组件库+全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 导入全局样式
+import './style/index.less'
+// 导入rem
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
-
+// 注册使用Vant组件库  Vue的要在import的下面书写！！
+Vue.use(Vant)
 new Vue({
   router,
   store,
